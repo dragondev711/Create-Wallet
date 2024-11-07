abf6e41459b2c29716f297eb1300269cb1a89c2a
MitsuiMinoru <mitsuimino711@gamil.com>
2024-11-07 17:06:44 +0900

    feat: Generate Seed

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ethers } from 'ethers'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface props { }
+
+const App: React.FC<props> = () => {
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  const GeneraticeWallet = async (): Promise<void> => {
+    const mnemonic = ethers.Wallet.createRandom().mnemonic;
+    console.log(mnemonic?.phrase);
+    
+    if (mnemonic && mnemonic.phrase) {
+      const wallet = ethers.Wallet.fromPhrase(mnemonic?.phrase)
+      console.log(wallet);
+      setOpenModal(true);
+    } else {
+      setOpenModal(false);
+      console.log("No wallet!");
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-[100vw] h-[100vh] flex justify-center items-center relative'>
+      <div className='z-1 w-[40vw] h-[30px] bg-[black] opacity-80 rounded-lg text-center' onClick={() => GeneraticeWallet()
+      }>
+        Create Wallet
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {
+        openModal &&
+        <div className='absolute z-1 w-[300px] h-[500px] bg-gray-900 border-2 rounded-[20px] flex flex-col gap-5 p-[10px]'>
+          <div className='w-full flex justify-center items-center relative text-[20px]'>
+            Create Wallet
+            <div className='absolute right-5 rounded-full text-white bg-gray-700 w-[20px] h-[20px] flex items-center justify-center text-[15px]' 
+                  onClick={() => setOpenModal(false)}>X</div>
+          </div>
+        </div>
+      }
+    </div>
   )
 }
 
